81d411a214180dd770e53e0f8c4ea64dc89fcfea
devyumi <yyumiwing@naver.com>
2024-03-27 21:42:29 +0900

    refactor: controller 불필요한 코드 삭제

--- src/main/java/com/example/signup/controller/SignInController.java
+++ src/main/java/com/example/signup/controller/SignInController.java
@@ -2,10 +2,8 @@ package com.example.signup.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("signin")
@@ -13,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SignInController {
 
     @GetMapping
-    public String signinOwn(@RequestParam(value = "error", required = false) String errorMessage, Model model) {
-        model.addAttribute("errorMessage", errorMessage);
+    public String signinOwn() {
         return "signinForm";
     }
 }
