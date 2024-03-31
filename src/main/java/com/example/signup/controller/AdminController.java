3252ae37726e4e7e32ceb7bb1be6ddfbeb509618
devyumi <yyumiwing@naver.com>
2024-03-31 16:32:01 +0900

    refactor: 미사용 코드 제거

--- src/main/java/com/example/signup/controller/AdminController.java
+++ src/main/java/com/example/signup/controller/AdminController.java
@@ -1,10 +1,8 @@
 package com.example.signup.controller;
 
-import com.example.signup.config.auth.CustomUserDetails;
 import com.example.signup.dto.MembersDto;
 import com.example.signup.service.AdminService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
